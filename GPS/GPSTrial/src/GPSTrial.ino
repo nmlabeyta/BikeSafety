3ad2255b3d247d395a73428483f1e9f561666460
unknown <benja.k.h@gmail.com>
2020-08-31 08:09:50 -0600

    gps trail works and hase oled dispay functions removed

---
+++ GPS/GPSTrial/src/GPSTrial.ino
@@ -0,0 +1,91 @@
+/*
+ * Project GPSTrial
+ * Description: Trial Program to Use TinyGPS++ on the Particle Argon with Display to OLED
+ * Author: Brian Rashap
+ * Date: 28-AUG-2020
+ */
+
+#include "Particle.h"
+#include "TinyGPS++.h"
+
+/*
+ * GPS Module Connections
+ * GPS Unit Rx connects to Argon Pin D9  (UART1_TX)
+ * GPS Unit Tx connects to Argon Pin D10 (UART1_RX)
+*/
+
+SYSTEM_THREAD(ENABLED);
+
+const unsigned long PUBLISH_PERIOD = 120000;
+const unsigned long SERIAL_PERIOD = 5000;
+const unsigned long MAX_GPS_AGE_MS = 10000; // GPS location must be newer than this to be considered valid
+
+// The TinyGPS++ object
+TinyGPSPlus gps;
+const int UTC_offset = -6; 
+unsigned long lastSerial = 0;
+unsigned long lastPublish = 0;
+unsigned long startFix = 0;
+bool gettingFix = false;
+
+float lat,lon,alt;
+
+void setup()
+{
+	Serial.begin(9600);
+
+	// The GPS module initialization
+	Serial1.begin(9600);
+    startFix = millis();
+    gettingFix = true;
+
+}
+
+void loop()
+{
+	while (Serial1.available() > 0) {
+		if (gps.encode(Serial1.read())) {
+			displayInfo();
+		}
+	}
+
+}
+
+void displayInfo() {
+	float lat,lon,alt;
+	uint8_t hr,mn,se;
+	if (millis() - lastSerial >= SERIAL_PERIOD) {
+		lastSerial = millis();
+
+		char buf[128];
+		if (gps.location.isValid() && gps.location.age() < MAX_GPS_AGE_MS) {
+			lat = gps.location.lat();
+			lon = gps.location.lng(); 
+			alt = gps.altitude.meters();
+			hr = gps.time.hour();
+			mn = gps.time.minute();
+			se = gps.time.second();
+
+			if(hr > 7) {
+				hr = hr + UTC_offset;
+			}
+			else {
+				hr = hr + 24 + UTC_offset;
+			}
+			Serial.printf("Time: %02i:%02i:%02i --- ",hr,mn,se);
+			Serial.printf("lat: %f, long: %f, alt: %f \n", lat,lon,alt);
+			if (gettingFix) {
+				gettingFix = false;
+				unsigned long elapsed = millis() - startFix;
+				Serial.printlnf("%lu milliseconds to get GPS fix", elapsed);
+			}
+		}
+		else {
+			strcpy(buf, "no location");
+			if (!gettingFix) {
+				gettingFix = true;
+				startFix = millis();
+			}
+		}
+	}
+}
